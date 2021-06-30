c28ecf6335eda732e6f34f9e323ec8e0725ce45c
Eranda Upeshitha <upeshitha@gmail.com>
2021-06-30 12:57:54 +0530

    Add auto mapper feature
    using auto mapper, it can automatically map with our database classes and view model classes.

--- Leave-management/Startup.cs
+++ Leave-management/Startup.cs
@@ -1,5 +1,7 @@
+using AutoMapper;
 using Leave_management.Contracts;
 using Leave_management.Data;
+using Leave_management.Mappings;
 using Leave_management.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -37,6 +39,8 @@ namespace Leave_management
             services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
             services.AddScoped<ILeaveHistoryRepository, LeaveHistoryRepository>();
             services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+
+            services.AddAutoMapper(typeof(Maps));
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
